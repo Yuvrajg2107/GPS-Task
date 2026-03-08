9d0d44a486cde7d063d93e291a61db3944fd4911
Yuvrajg2107 <yuvrajgandhmal@gmail.com>
2026-03-08 23:45:06 +0530

    api

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Create an Axios instance with your backend URL
 const API = axios.create({
-    baseURL: 'https://localhost:5000/api',
+    baseURL: 'https://gps-task-backend.onrender.com/api',
 });
 
 // Interceptor: Automatically adds the "Authorization" header to every request
